2c760e9c85bc89d3120dd9e73e0c01bb5058a1cb
ANKUSH DONGRE <dongreankush52@gmail.com>
2023-06-02 11:04:07 +0530

    added images and components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Services from './components/Services'
+import Tours from './components/Tours'
+import Footer from './components/Footer'
+
+
 function App() {
   return (
-    <h1>App</h1>
+    <>
+    <Navbar />
+    <Hero />
+    <About />
+    <Services />
+    <Tours />
+    <Footer />
+    </>
   );
 }
 
